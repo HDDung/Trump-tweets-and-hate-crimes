268e545c20cacc05eba687196f259ba0bb9024f2
dhuynh <cs2014_dung.hd@student.vgu.edu.vn>
2020-06-05 19:24:32 +0200

    [Dung] Data Collection: FBI master data

---
+++ scripts/data-collection/01 - FBI-data-collection-FBI-master-data.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(readr)
+library(ggplot2)
+
+hate_crime <- read_csv("raw-data/hate_crime.csv")
+hate_crime$INCIDENT_DATE <- dmy(hate_crime$INCIDENT_DATE)
+hate_crime$BIAS_DESC <- factor(hate_crime$BIAS_DESC)
+year(hate_crime$DATA_YEAR) 
+
+
+
+incident_num_weekly <- 
+  hate_crime %>% 
+  select(DATA_YEAR, 
+         INCIDENT_DATE, 
+         STATE_ABBR, 
+         BIAS_DESC, 
+         OFFENDER_RACE) %>%
+  filter(grepl('Anti-Hispanic or Latino', BIAS_DESC)) %>%
+  arrange(DATA_YEAR, INCIDENT_DATE) %>%
+  group_by(week = cut(INCIDENT_DATE, "week"), state = STATE_ABBR) %>%
+  summarise(total = n())
+
+ggplot(incident_num_weekly[incident_num_weekly$state == 'SD',], aes(x = week, y = total)) +
+  geom_line(group = 1)
+
+incident_num_yearly <- 
+  hate_crime %>% 
+  select(DATA_YEAR, 
+         STATE_ABBR, 
+         BIAS_DESC, 
+         OFFENDER_RACE) %>%
+  filter(grepl('Anti-Hispanic or Latino', BIAS_DESC)) %>%
+  arrange(DATA_YEAR) %>%
+  group_by(year = DATA_YEAR, state = STATE_ABBR) %>%
+  summarise(total = n())
+
+incident_num_yearly
+
+
+ggplot(incident_num_yearly[incident_num_yearly$state == 'SD',], aes(x = year, y = total, col=state)) +
+  geom_bar(stat = "identity")
+
+
+View(hate_crime %>% 
+  select(DATA_YEAR, 
+         BIAS_DESC,
+         AGENCY_TYPE_NAME) %>%
+    filter(grepl('County', AGENCY_TYPE_NAME)) %>%
+    arrange(DATA_YEAR) %>%
+    group_by(year = DATA_YEAR) %>%
+    summarise(total = n()))
+
+tmp <- hate_crime %>%
+  unite(ID, INCIDENT_ID,ORI, remove = FALSE, sep = "_")
+length(unique(tmp[tmp$DATA_YEAR == 2018,]$ID))
